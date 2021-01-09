93cec4fd9f269ec5e8140d6755051013f54381d6
JoaoVictorPereiraTeixeira <joaovictorteixeira9819@gmail.com>
2021-01-09 14:40:38 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css'
 import Routes from './Routes';
 
@@ -7,6 +9,7 @@ function App() {
   return (
     <>
       <Routes/>
+      <ToastContainer/>
     </>
   );
 }
